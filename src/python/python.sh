12521b47442274c2ef036f9f80166fad8486e5d6
ikey4u <pwnkeeper@gmail.com>
2023-06-04 22:34:38 +0800

    feat: clone pyenv only when it does not exist

--- src/python/python.sh
+++ src/python/python.sh
@@ -1,11 +1,8 @@
 function python_install() {
-    if [[ -d "${HOME}/.pyenv" ]]; then
-        return 0
-    fi
-
-    git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-    if ! grep -q LIZSYS_PYENV_MARK ${HOME}/.bashrc; then
-        cat >> ${HOME}/.bashrc <<EOF
+    if [[ ! -d "${HOME}/.pyenv" ]]; then
+        git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+        if ! grep -q LIZSYS_PYENV_MARK ${HOME}/.bashrc; then
+            cat >> ${HOME}/.bashrc <<EOF
 # LIZSYS_PYENV_MARK
 export PYENV_ROOT="\$HOME/.pyenv"
 export PATH="\$PYENV_ROOT/bin:\$PATH"
@@ -13,6 +10,7 @@ if command -v pyenv 1>/dev/null 2>&1; then
     eval "\$(pyenv init --path)"
 fi
 EOF
+        fi
     fi
 
     local deps_done=NO
@@ -28,9 +26,12 @@ EOF
     fi
     if [[ "${deps_done}" == NO ]]; then
         echo "[x] failed to install dependencies"
-    else
-        PYTHON_CONFIGURE_OPTS="--enable-shared" ${HOME}/.pyenv/bin/pyenv install 3.9.5
+        return 1
     fi
+
+    local pyver=3.9.5
+    PYTHON_CONFIGURE_OPTS="--enable-shared" ${HOME}/.pyenv/bin/pyenv install ${pyver}
+    pyenv global 3.9.5
 }
 
 python_install
