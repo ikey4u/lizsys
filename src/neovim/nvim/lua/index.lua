3dd6e3984ddbe93772f7ca536428df0167053889
ikey4u <pwnkeeper@gmail.com>
2023-07-31 10:09:23 +0800

    fix: print kotlin lsp warning only when ANDROID_JDK_DIR does not exist

--- src/neovim/nvim/lua/index.lua
+++ src/neovim/nvim/lua/index.lua
@@ -146,15 +146,16 @@ lsp.bashls.setup({
 --
 -- For android development, you may export the java bundled with Android Studio into PATH,
 -- or else neovim will use the system java which could not work with kotlin.
-if vim.env.ANDROID_JDK_DIR == nil then
+if vim.env.ANDROID_JDK_DIR ~= nil then
+    lsp.kotlin_language_server.setup({
+        cmd_env = {
+            PATH = vim.env.ANDROID_JDK_DIR .. "/bin:" .. vim.env.PATH,
+            JAVA_HOME = vim.env.ANDROID_JDK_DIR,
+        },
+    })
+else
     print("ANDROID_JDK_DIR is not set, your kotlin_language_server may not work")
 end
-lsp.kotlin_language_server.setup({
-    cmd_env = {
-        PATH = vim.env.ANDROID_JDK_DIR .. "/bin:" .. vim.env.PATH,
-        JAVA_HOME = vim.env.ANDROID_JDK_DIR,
-    },
-})
 
 -- lsp.vimscript
 lsp.vimls.setup({
